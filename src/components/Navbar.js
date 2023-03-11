5c8d902b8c0024bdb229ce585bba59f1a144cb2d
ltk2801 <luutuankhanha3dt@gmail.com>
2023-03-12 02:43:46 +0700

    complete navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import styles from "./Navbar.module.css";
+import SearchIcon from "./SerchIcon.js";
+
+const Navbar = (props) => {
+  const navigate = useNavigate();
+  const [isTransparent, setIsTransparent] = useState(true);
+
+  const homeHandleClick = () => {
+    navigate("/");
+  };
+  const searchHandlerClick = () => {
+    navigate("/search");
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPosition = window.pageYOffset;
+      if (currentScrollPosition >= 100) {
+        setIsTransparent(false);
+      } else {
+        setIsTransparent(true);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const navbarClasses = isTransparent
+    ? `${styles.navbar} + ${styles.transparent} `
+    : `${styles.navbar} + ${styles["not-transparent"]} `;
+
+  return (
+    <div className={navbarClasses}>
+      <h3 className={styles["navbar-brand"]} onClick={homeHandleClick}>
+        Movie App
+      </h3>
+      <SearchIcon
+        className={styles["navbar-icon"]}
+        onClick={searchHandlerClick}
+      />
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
+import React from "react";
+import Navbar from "../../components/Navbar";
 const Search = () => {
-	return (
-		<div className='app'>
-			<h1>Search</h1>
-		</div>
-	);
+  return (
+    <div className="app">
+      <Navbar />
+    </div>
+  );
 };
 
 export default Search;
